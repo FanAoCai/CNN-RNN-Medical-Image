d37dc3e10c626d4d9bc9999da1c3691836052ba9
FanAoCai <1846109672@qq.com>
2024-01-04 20:40:16 +0800

    Code of WSI

---
+++ README.md
@@ -0,0 +1,141 @@
+# <div align="center"> 代码与结果的简单介绍</div>
+
+## openslide函数的相关用法
+使用openslide对WSI（全扫描）图像进行处理，下面是openslide函数的相关用法：
+
+```python
+image = openslide.OpenSlide(silidepath)   #读取WSI图像，slidepath为图像的路径
+level = image.level_count   #WSI图像的层级
+rvl = image.level_dimensions[i]   #输出i层的图像分辨率
+downsample_image = image.level_downsamples[i]   #对i层的图像进行下重采样
+best_downsample_level = image.get_best_level_for_downsample(5.0)   #对给定的下采样因子返回图像下采样的级别
+result = np.array(image.read_region((0, 0), 3, (224, 224)))   #(0,0）代表起始像素点，3代表需要处理的层级，（224,224)代表处理得到的像素大小
+```
+### 解决思路流程图
+
+整体的解决思路为：首先训练图像分割模型，以ResNet34为基础模型进行训练，先使用ResNet对每张WSI每个层级图像进行扫描，给出该切片是癌症的概率，接着将每张WSI最高概率的切片送入模型，更新ResNet的参数，下面是模型架构：
+
+<div align="center">
+<img src="source_image/CNN_arch.png" width="450"/>
+</div>
+
+接着使用训练好的ResNet模型，扫描WSI图像的每个层级图像，得到S个病变概率最大的区域送入RNN模型进行训练，更新参数，下面是RNN架构图：
+<div align="center">
+<img src="source_image/RNN_arch.png" width="450"/>
+</div>
+
+## 病理切片可视化
+对淋癌病理切片 `2023-11-21 12_18_01.svs_0.png` 进行可视化，该癌症切片共有`7`个级别，其分辨率从大到小分别为`[(88084, 39276),(22021, 9819),(5505, 2454),(1376, 613),(344, 153),(86, 38),(21, 9)]`,下面对该切片`3-5`三个层级进行展示：
+
+```python
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+plt.imshow(result)   #result为read_region方法返回的图像数组
+plt.savefig(image_path)   #iamge_paath为存储路径
+plt.show()
+```
+
+* `"级别3"`: 分辨率为：`(1376, 613)`
+
+<div align="center">
+<img src="WSI_show/cancer_1/2023-11-21 12_18_01/level_3_change.png" width="450"/>
+</div>
+
+* `级别4`: 分辨率为：`(344, 153)`
+<div align="center">
+<img src="WSI_show/cancer_1/2023-11-21 12_18_01/level_4_change.png" width="450"/>
+</div>
+
+* `级别5`: 分辨率为：`(86, 38)`
+<div align="center">
+<img src="WSI_show/cancer_1/2023-11-21 12_18_01/level_5_change.png" width="450"/>
+</div>
+
+
+
+## 数据处理
+WSI图像作为输入时，其层级必须保持一致，由于数据集的限制，故从以下两个图像层级角度进行研究和对比，分别是图像层级设置为3和7，训练集、验证集和测试集的比例均为`6:2:2`，targets代表数据的标签，0为正常切片，1为癌症切片，下面为数据集的结构：
+```python
+XXX_data = {
+        'slides': slides[:tr_0] + slides[database:database+tr_1], #WSI的存储路径
+        'grid': grid[:tr_0] + grid[database:database+tr_1], #WSI的每一级图像分辨率大小
+        'targets': targets[:tr_0] + targets[database:database+tr_1], #WSI图像的标签，0表示正常切片，1表示癌症切片
+        'mult': mult, #图像的缩放比例，默认为1，从而决定是否要插值
+        'level': level, #WSI图像的起始层级，默认为0
+        }
+```
+
+* `level（7）`：处理的到的数据两位train ：val ：test = 14 : 4 : 7，训练、验证和测试集中均有癌症切片和正常切片，其中`targets`的分布分别为：
+
+```python
+train：[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+val：  [0, 1, 1, 1]
+test： [0, 0, 1, 1, 1, 1, 1]
+```
+* `level（3）`：train ：val ：test = 30 : 9 : 12，训练、验证和测试集中均有癌症切片和正常切片，其中`targets`的分布分别为：
+
+```python
+train：[0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+val：  [0, 0, 1, 1, 1, 1, 1, 1, 1]
+test： [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+```
+## 训练结果
+Resnet模型和RNN模型均训练100个epoch，统计训练过程中的loss以及在测试集上的准确率，下面为详细的结果：
+
+### 层级为7
+
+RseNet模型:
+
+* `loss曲线`：
+<div align="center">
+<img src="source_image/WSI_CNN_L7_loss.png" width="350"/>
+</div>
+
+* `测试准确率`：71%
+<div align="center">
+<img src="source_image/prediction_CNN_L7.png" width="350"/>
+</div>
+
+RNN模型:
+
+* `loss曲线`：
+随着batch_size的增大，模型的loss降低，当batch_size过小时，出现nan值，但受限于数据量，batch无法增大，虽然loss保持稳定，但是相对值较大，只能通过增加数据量解决。
+<div align="center">
+<img src="source_image/WSI_RNN_L7_loss.png" width="350"/>
+</div>
+
+* `测试准确率`：71%
+<div align="center">
+<img src="source_image/prediction_RNN_L7.png" width="350"/>
+</div>
+
+
+
+
+### 层级为3
+
+RseNet模型:
+
+* `loss曲线`：
+<div align="center">
+<img src="source_image/WSI_CNN_L3_loss.png" width="350"/>
+</div>
+
+* `测试准确率`：50%
+<div align="center">
+<img src="source_image/prediction_CNN_L3.png" width="350"/>
+</div>
+
+RNN模型:
+
+* `loss曲线`：
+随着batch_size的增大，模型的loss降低，当batch_size过小时，出现nan值，但受限于数据量，batch无法增大，虽然loss保持稳定，但是相对值较大，只能通过增加数据量解决,loss表现的极其不稳定，且容易出现过拟合情况，原因为数据量太少。
+<div align="center">
+<img src="source_image/WSI_RNN_L3_loss.png" width="350"/>
+</div>
+
+* `测试准确率`：42%
+<div align="center">
+<img src="source_image/prediction_RNN_L3.png" width="350"/>
+</div>
---
+++ test.py
@@ -0,0 +1,219 @@
+import openslide
+import numpy as np
+import scipy.misc
+import matplotlib.pyplot as plt
+import pylab
+import os
+import torch
+from tqdm import tqdm, trange
+
+from PIL import Image
+# import moxing as mox
+import pandas as pd
+import cv2
+# import moxing as mox
+
+#Parameter Definition
+def get_data_from_obs():
+    #癌症切片
+    data = mox.file.list_directory('obs://wzclinai1/', recursive=True)
+    for name in tqdm(data):
+        mox.file.copy('obs://wzclinai1/' + name,'/home/ma-user/work/data/cancer/' + name)
+
+    #正常切片
+    # name = '2023-11-01 19_11_18.svs'
+    # mox.file.copy('obs://migrationbackup/zcqp/' + name,'/home/ma-user/work/data/cancer_free/' + name)
+    data = mox.file.list_directory('obs://migrationbackup/zcqp/', recursive=True)
+    for name in tqdm(data):
+        try:
+            mox.file.copy('obs://migrationbackup/zcqp/' + name,'/home/ma-user/work/data/cancer_free/' + name)
+        except:
+            continue
+    
+    print(data)
+    with open('../../obs_data.txt') as f:
+        data = f.read()
+        data_list = data.split('<Key>')
+        print(data_list)
+
+    mox.file.copy('obs://wzclinai1','/home/ma-user/work/data/wzclinai1')
+    filelist = os.listdir('obs://wzclinai1/')
+    for file in filelist:
+        print(file)
+
+def collect_dataset():
+    '''
+    image.level_count: View the number of image level, level refers to the hierarchy of the image
+    image.level_downsamples: Downscaling a level of a WSI
+    '''
+    slides = []
+    grid = []
+    targets = []
+    mult = 1
+    level = 0
+
+    number_data = []
+    #Process prepare the train and test data
+    filedir = ('data')
+    filelist = os.listdir(filedir)
+    
+    for file in filelist:
+        subfilelist = os.listdir(filedir + '/' +file)
+        data_count = 0
+        for i, subfile in tqdm(enumerate(subfilelist)):
+            # print(subfile)
+            image = openslide.OpenSlide(filedir + '/' + file + '/' + subfile)
+            if image.level_count < 3:
+                continue
+            # if image.level_count != 7:
+            #     continue
+            # print(os.path.abspath(subfile).replace('\\', '/')) 
+            slides.append(filedir + '/' + file + '/' + subfile)
+            # print(image.level_downsamples[0])
+            # print(image.get_best_level_for_downsample(98932))
+            subgrid = []
+            # print(image.level_count)
+            # for j in range(image.level_count):
+            #     subgrid.append(image.level_dimensions[j])
+
+            #最低级别为3，因此全部设定为3
+            for j in range(3):
+                subgrid.append(image.level_dimensions[j])
+                # print(image.level_dimensions[j])
+            grid.append(subgrid)
+            # print(file.split('_')[1])
+            targets.append(int(file.split('_')[-1]))
+            data_count += 1
+        number_data.append(data_count)
+
+    return slides, grid, targets, mult, level, number_data
+
+def set_train_val_test_dataset(train_split, val_split):
+    slides, grid, targets, mult, level, number_data = collect_dataset()
+    test_split = 1 - train_split - val_split
+    train_data_split = list(map(lambda x:int(x*train_split),number_data))
+    val_data_split = list(map(lambda x:int(x*val_split),number_data))
+    test_data_split = list(map(lambda x:int(x*test_split),number_data))
+
+    database = number_data[0]
+    tr_0 = train_data_split[0]
+    tr_1 = train_data_split[1]
+
+    train_data = {
+        'slides': slides[:tr_0] + slides[database:database+tr_1],
+        'grid': grid[:tr_0] + grid[database:database+tr_1],
+        'targets': targets[:tr_0] + targets[database:database+tr_1], 
+        'mult': mult,
+        'level': level,
+        }
+    print('train_data : ', len(train_data['slides']))
+    print(train_data['targets'])
+    
+    va_0 = val_data_split[0]
+    va_1 = val_data_split[1]
+
+    val_data = {
+        'slides': slides[tr_0:tr_0+va_0] + slides[database+tr_1:database+tr_1+va_1],
+        'grid': grid[tr_0:tr_0+va_0] + grid[database+tr_1:database+tr_1+va_1],
+        'targets': targets[tr_0:tr_0+va_0] + targets[database+tr_1:database+tr_1+va_1], 
+        'mult': mult,
+        'level': level,
+        }
+    print('val_data : ', len(val_data['slides']))
+    print(val_data['targets'])
+
+    test_data = {
+        'slides': slides[tr_0+va_0:database] + slides[database+tr_1+va_1:],
+        'grid': grid[tr_0+va_0:database] + grid[database+tr_1+va_1:],
+        'targets': targets[tr_0+va_0:database] + targets[database+tr_1+va_1:], 
+        'mult': mult,
+        'level': level,
+    }
+    print('test_data : ', len(test_data['slides']))
+    print(test_data['targets'])
+    
+    #所有图像全部输入
+    # if not os.path.exists('origin_data/'):
+    #     os.mkdir('origin_data/')
+    # torch.save(train_data, 'origin_data/train_data')
+    # torch.save(val_data, 'origin_data/val_data')
+    # torch.save(test_data, 'origin_data/test_data')
+
+    #仅使用级别为7的图像
+    # if not os.path.exists('7cls_data/'):
+    #     os.mkdir('7cls_data/')
+    # torch.save(train_data, '7cls_data/train_data_7cls')
+    # torch.save(val_data, '7cls_data/val_data_7cls')
+    # torch.save(test_data, '7cls_data/test_data_7cls')
+
+    #仅使用图像的3个级别
+    if not os.path.exists('3cls_data/'):
+        os.mkdir('3cls_data/')
+    torch.save(train_data, '3cls_data/train_data_3cls')
+    torch.save(val_data, '3cls_data/val_data_3cls')
+    torch.save(test_data, '3cls_data/test_data_3cls')
+
+    print('Finish saving train_data val_data and test_data!!!')
+
+def open_image_svs():
+    path = 'cancer-free_0/2023-11-21 15_23_38.svs'
+    image = openslide.OpenSlide('data/' + path)
+    print(image.level_count)
+    for i in range(image.level_count):
+        print(image.level_dimensions[i])
+        
+    #(0,0）代表起始像素点，2代表需要处理的层级，（224,224)代表处理得到的像素大小
+    # result = np.array(image.read_region((0, 0), 3, (224, 224)))
+    if not os.path.exists('WSI_show/' + path.split('.')[0]):
+            os.mkdir('WSI_show/' + path.split('.')[0])
+    for i in range(3):
+        result = np.array(image.read_region((0, 0), i+3, image.level_dimensions[i+3]).convert('RGB'))
+        print(result.shape)
+        plt.figure(i)
+        plt.imshow(result)
+        # plt.savefig('WSI_show/' + path.split('.')[0] + '/level_' + str(i+3) +'.png')
+        plt.show()
+
+def count_number():
+    print()
+
+def main():
+    image = openslide.OpenSlide('data/cancer_1/2023-11-21 12_18_01.svs')
+    # print(image)
+    # data = torch.load('train_data')
+    # print(data)
+    # print(type(data))
+    # slides_train = torch.load('train_data')
+    # print(slides_train)
+    # train_data = pd.DataFrame(slides_train['slides'])
+    # torch.save(slides['slides'], 'train_data.txt')
+    # slides_val = torch.load('val_data')
+    # print(slides_val)
+    # val_data = pd.DataFrame(slides_val['slides'])
+    # train_data.to_csv('train_data.csv')
+    # val_data.to_csv('val_data.csv')
+    # torch.save(slides['slides'], 'val_data.txt')
+    # pred = [1,1,1,1]
+    # real = [1,1,1,1]
+    # pred = np.array(pred)
+    # real = np.array(real)
+    # neq = np.not_equal(pred, real)
+    # err = float(neq.sum())/pred.shape[0]
+    # print(pred)
+    # print(real)
+    # print(neq.sum())
+    # fpr = float(np.logical_and(pred==1,neq).sum())/(real==0).sum()
+    # print(np.logical_and(pred==1,neq))
+
+    # fnr = float(np.logical_and(pred==0,neq).sum())/(real==1).sum()
+
+
+
+
+if __name__ == '__main__':
+    # collect_dataset()
+    set_train_val_test_dataset(0.6, 0.2)
+    # set_test_dataset()
+    # get_data_from_obs()
+    # open_image_svs()
+    # main()
